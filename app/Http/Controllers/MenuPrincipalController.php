64da6bfcb43c00a0cb9e3fc55f364ddf02ad9fb3
FilippieGit <filippieamaral@gmail.com>
2025-02-25 19:55:01 -0300

    projetoLaravel inicial

---
+++ app/Http/Controllers/MenuPrincipalController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MenuPrincipalController extends Controller
+{
+    public function menuprincipal(){
+        
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/contato', \app\Http\Controllers\ContatoController::class, contato);
+
